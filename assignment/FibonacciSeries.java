220040c888e0b004767954127882ff5549ea67db
SrinivasPV <117560006+SrinivasPV@users.noreply.github.com>
2022-11-07 19:31:43 +0530

    Add files via upload
    correct fibnacci series upload

--- assignment/FibonacciSeries.java
+++ assignment/FibonacciSeries.java
@@ -5,14 +5,15 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a=8;
+		int a=10;
 		int first=0;
 		int sec = 1;
-		int sum=0;
-		System.out.printf("%d ",sum);
+		
+		System.out.printf("%d %d ",first,sec);
 		
 		for(int i= 1;i<a-1;i++)
 		{
+			int sum;
 			sum=first+sec;
 			first=sec;
 			sec=sum;
