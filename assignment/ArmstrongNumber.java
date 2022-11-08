f5bb310b41652f994ee4c000dfdad795797e9897
SrinivasPV <117560006+SrinivasPV@users.noreply.github.com>
2022-11-08 20:22:39 +0530

    Add files via upload
    updated armstrong code

--- assignment/ArmstrongNumber.java
+++ assignment/ArmstrongNumber.java
@@ -6,22 +6,28 @@ public class ArmstrongNumber {
 		// TODO Auto-generated method stub
 
 		int a=153;
+		int rem=0;
+		int c=a;
 		int b;
-		int c;
-		int d;
+		//int c;
+		//int d;
 		
 		while(a>0)
 		{
 			b=a%10;
+			rem=rem+(b*b*b);
 			a=a/10;
-			c=a%10;
+			/*c=a%10;
 			a=a/10;
 			d=a%10;
-			System.out.println((d*d*d)+(c*c*c)+(b*b*b));
-			a=a/10;
+			a=a/10; */
 		}
+		if(c == rem)
 		
+		System.out.println(rem + " is an Armstrong number");
 		
+		else
+			System.out.println(rem + " is not an Armstrong number");
 	}
 
 }
